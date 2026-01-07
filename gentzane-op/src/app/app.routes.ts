ea8fdec214b23e5e5e291d802fc54804539d4b40
gentzane99 <gentzaneop@gmail.com>
2026-01-07 13:49:29 +0100

    rutas base echas y header en proceso

--- gentzane-op/src/app/app.routes.ts
+++ gentzane-op/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { GamesComponent } from './pages/games/games.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { EducationComponent } from './pages/education/education.component';
+import { DemoComponent } from './pages/demo/demo.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { NotFoundComponent } from './pages/error/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'games', component: GamesComponent},
+    {path: 'projects', component: ProjectsComponent},
+    {path: 'education', component: EducationComponent},
+    {path: 'demo', component: DemoComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: '**', component: NotFoundComponent}
+];
