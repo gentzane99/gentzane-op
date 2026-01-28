b541f680a4ad6bca61f7c7c41407e94e9a54fd6c
gentzane99 <gentzaneop@gmail.com>
2026-01-28 18:17:45 +0100

    working on projects page

--- gentzane-op/src/app/pages/projects/projects.component.ts
+++ gentzane-op/src/app/pages/projects/projects.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { ProjectsService } from '../../services/projects.service';
-import { IProject } from '../../interfaces/iproject.interface';
+import { IProject, DigipenCategory, UNIRCategory } from '../../interfaces/iproject.interface';
 import { ProjectsCardComponent } from "./projects-card/projects-card.component";
 
 @Component({
@@ -15,6 +15,29 @@ export class ProjectsComponent {
     unirProjects: IProject[] = [];
     digiPenProjects: IProject[] = [];
 
+    unirOrder: UNIRCategory[] = ['Frontend', 'Backend', 'Database', 'Fullstack'];
+    digipenOrder: DigipenCategory[] = ['CS120', 'CS250', 'CS300', 'CS380'];
+    digipenCourseNames: Record<DigipenCategory, string> = {
+      CS120: 'High-Level Programming',
+      CS250: 'Computer Graphics',
+      CS300: 'Advanced Computer Graphics',
+      CS380: 'AI',
+    };
+
+    unirByCourse: Record<UNIRCategory, IProject[]> = {
+      Frontend: [],
+      Backend: [],
+      Database: [],
+      Fullstack: [],
+    };
+
+    digipenByCourse: Record<DigipenCategory, IProject[]> = {
+      CS120: [],
+      CS250: [],
+      CS300: [],
+      CS380: [],
+    };
+
     loading = true; 
     error: string | null = null;
 
@@ -23,6 +46,17 @@ export class ProjectsComponent {
             this.projects = await this.projectService.getProjects();
             this.unirProjects = this.projects.filter(p => p.category === 'UNIR');
             this.digiPenProjects = this.projects.filter(p => p.category === 'DigiPen');
+
+             // Agrupar UNIR
+            for (const c of this.unirOrder) {
+              this.unirByCourse[c] = this.unirProjects.filter(p => p.course === c);
+            }
+        
+            // Agrupar DigiPen
+            for (const c of this.digipenOrder) {
+              this.digipenByCourse[c] = this.digiPenProjects.filter(p => p.course === c);
+            }
+
         } catch (error) {
             this.error = 'Error al cargar los proyectos';
             console.error(this.error, error)
