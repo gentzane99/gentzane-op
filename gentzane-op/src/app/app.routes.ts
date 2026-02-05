ff980383f37d8a985220320665bcc0a12acaf73c
Gentzane Pastor Oleagoitia <gentzaneop@gmail.com>
2026-02-05 12:33:46 +0100

    added skills

--- gentzane-op/src/app/app.routes.ts
+++ gentzane-op/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { EducationComponent } from './pages/education/education.component';
 import { DemoComponent } from './pages/demo/demo.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { NotFoundComponent } from './pages/error/not-found/not-found.component';
+import { SkillsComponent } from './pages/skills/skills.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
@@ -13,6 +14,7 @@ export const routes: Routes = [
     {path: 'games', component: GamesComponent},
     {path: 'projects', component: ProjectsComponent},
     {path: 'education', component: EducationComponent},
+    {path: 'skills', component: SkillsComponent},
     {path: 'demo', component: DemoComponent},
     {path: 'contact', component: ContactComponent},
     {path: '**', component: NotFoundComponent}
