519f0c4da35e93080bc4f6e57eeda68a8a1176c0
Gentzane Pastor Oleagoitia <gentzaneop@gmail.com>
2026-02-10 17:58:15 +0100

    skills done y contacto provisional

--- gentzane-op/src/app/pages/skills/skills.component.ts
+++ gentzane-op/src/app/pages/skills/skills.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ILanguages, ISkills } from '../../interfaces/iskills.interfaces';
+import { SkillsService } from '../../services/skills.service';
 
 @Component({
   selector: 'app-skills',
@@ -7,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.css',
 })
 export class SkillsComponent {
+    private skillsService = inject(SkillsService);
 
+    skills: ISkills | null = null;
+    techSkills: string[] = [];
+    softSkills: string[] = [];
+    languages: ILanguages[] = [];
+
+    loading = true; 
+    error: string | null = null;
+    
+    async ngOnInit(){
+
+        try{
+            this.skills = await this.skillsService.getSkills();
+            this.techSkills = this.skills.technical;
+            this.softSkills = this.skills.soft;
+            this.languages = this.skills.languages;
+
+        } catch (error) {
+            this.error = 'Error al cargar los proyectos';
+            console.error(this.error, error)
+        }
+        this.loading = false;
+    }
 }
